92308b43368d9ecd9191cfd2ce0ff56dbe8df4b7
tung <nguyenhoangtung1610@gmail.com>
2024-11-04 21:35:29 +0100

    define abstraction layer for CPTS

---
+++ Main_Program/cpts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>        // For open()
+#include <sys/mman.h>     // For mmap()
+#include "cpts.h"
+//Allocate memory
+volatile uint32_t *cpts_Base = NULL ;
+void setupCPTS()
+{
+    int mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (mem_fd < 0)
+    {
+        perror("Failed to open /dev/mem");
+        exit(EXIT_FAILURE);
+    }
+    volatile uint32_t *cpts_Base=(uint32_t*) mmap(NULL, 0x1000, PROT_READ|PROT_WRITE, MAP_SHARED, mem_fd, 0x4A100000 );
+    cpts_Base[(CPTS_CONTROL_REG-CPTS_BASE)/4]=0xF01;
+    cpts_Base[(CPTS_INT_ENABLE_REG-CPTS_BASE)/4]=0x1;
+}
